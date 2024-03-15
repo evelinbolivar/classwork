560958e3ca999d8e57c795b55213f311583eae0b
evelin <ebolivarsaldarriaga@bostonk12.org>
2024-03-15 09:59:41 -0400

    tarea 3/15/2024

---
+++ week_13/about_me.py
@@ -0,0 +1,7 @@
+# Evelin Bolivar
+#03/15/2024
+
+print("I was born in the city of Medellin.")
+print("I was born on Colombia.")
+print("I have 16 years.")
+print("my favorite academic class is Math.")
